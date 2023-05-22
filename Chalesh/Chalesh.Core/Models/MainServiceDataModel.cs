58269461771a4020783fc3372ae6d90607bd8f98
mr-mohammadi <mr-mohammadi@amnpardaz.com>
2023-05-22 21:18:39 +0430

    add Consumer, MainService, Producer Model

---
+++ Chalesh/Chalesh.Core/Models/MainServiceDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chalesh.Core.Models
+{
+    public class MainServiceDataModel
+    {
+        public Guid Id { get; set; }
+        public DateTime SystemTime { get; set; }
+        public int NumberofConnectedClients { get; set; }
+    }
+}
---
+++ Chalesh/Chalesh.Core/Models/ProducerModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chalesh.Core.Models
+{
+    public class ProducerModel
+    {
+        public int Id { get; set; }
+        public int MessageLenght { get; set; }
+        public string? Engine { get; set; }
+        public bool IsValid { get; set; }
+
+    }
+}
