6fee40964b0537fb8bcfa010f90fae655b42db2a
mr-mohammadi <mr-mohammadi@amnpardaz.com>
2023-05-23 13:11:00 +0430

    update main service data model

--- Chalesh/Chalesh.Core/Models/MainServiceDataModel.cs
+++ Chalesh/Chalesh.Core/Models/MainServiceDataModel.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace Chalesh.Core.Models
 {
-    public class MainServiceDataModel
+    public class MainServiceDataModelIn
     {
         public Guid Id { get; set; }
         public DateTime SystemTime { get; set; }
         public int NumberofConnectedClients { get; set; }
+    }
+    public class MainServiceDataModelOut
+    {
+        public bool IsEnabled { get; set; }
+        public DateTime ExpirationTime { get; set; }
+        public int NumberOfActiveClients { get; set; }
     }
 }
