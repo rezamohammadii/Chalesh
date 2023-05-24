c4f407264ee2ad857b1d713400ed608ac56dbeb9
mr-mohammadi <mr-mohammadi@amnpardaz.com>
2023-05-24 14:43:53 +0430

    add Service2DetailModel

---
+++ Chalesh/Chalesh.Core/Models/Service2DetailModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chalesh.Core.Models
+{
+    public class Service2DetailModel
+    {
+        public string? Id { get; set; }
+        public string? Type { get; set; }
+    }
+}
