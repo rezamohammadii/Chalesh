96611785f4f2dde6dc91141521a6921a668296bf
mr-mohammadi <mr-mohammadi@amnpardaz.com>
2023-05-28 16:08:43 +0330

    add strore interface instead public global varibale

---
+++ Chalesh/Chalesh.Core/Services/IStoreData.cs
@@ -0,0 +1,18 @@
+﻿using Chalesh.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chalesh.Core.Services
+{
+    public interface IStoreData
+    {
+        void StoreMainServiceDataOut(MainServiceDataModelOut? modelOut);
+        MainServiceDataModelOut GetMainServiceDataOut();
+        void StoreService2DetailOut(Service2DetailModel modelOut);
+        void StoreConsumerModel(ConsumerModel? consumerModel);
+        ConsumerModel GetConsumerModel();
+    }
+}
---
+++ Chalesh/Chalesh.Core/Services/StoreDataService.cs
@@ -0,0 +1,45 @@
+﻿using Chalesh.Core.Models;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chalesh.Core.Services
+{
+    public class StoreDataService : IStoreData
+    {
+        private static MainServiceDataModelOut? ModelOut;
+        private static ConcurrentQueue<Service2DetailModel>? Service2Detail;
+        private static ConsumerModel? ConsumerModels;
+        public void StoreConsumerModel(ConsumerModel? consumerModel)
+        {
+            ConsumerModels.Message  = consumerModel.Message;
+            ConsumerModels.Sender  = consumerModel.Sender;
+            ConsumerModels.Id  = consumerModel.Id;
+        }
+
+        public void StoreMainServiceDataOut(MainServiceDataModelOut? modelOut)
+        {
+            ModelOut.IsEnabled = modelOut.IsEnabled;
+            ModelOut.ExpirationTime = modelOut.ExpirationTime;
+            ModelOut.NumberOfActiveClients = modelOut.NumberOfActiveClients;
+        }
+
+        public MainServiceDataModelOut GetMainServiceDataOut()
+        {
+            return ModelOut!;
+        }
+
+        public void StoreService2DetailOut(Service2DetailModel? modelOut)
+        {
+            Service2Detail.Enqueue(modelOut);
+        }
+
+        public ConsumerModel GetConsumerModel()
+        {
+            return ConsumerModels!;
+        }
+    }
+}
